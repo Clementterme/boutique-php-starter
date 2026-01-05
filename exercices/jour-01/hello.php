206920500f0c752410c8518c3f78833f14473f68
Clementterme <clement.terme.simplon@gmail.com>
2026-01-05 15:10:25 +0100

    Exercice 2 terminé

--- exercices/jour-01/hello.php
+++ exercices/jour-01/hello.php
@@ -1,1 +1,20 @@
-<?php echo "Hello PHP!!" ?>
+<?php
+
+echo "Hello PHP!!";
+
+$name = "Bouteille";
+$price = 3.99;
+$stock = 42;
+$onSale = true;
+
+echo "<br />";
+
+var_dump($name,$price,$stock,$onSale);
+
+echo "<br />";
+
+echo "Le produit " . $name . " coûte " . $price . " €.";
+
+echo "<br />";
+
+?>
