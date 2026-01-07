ad6046ad75aeacc7f3030d0fec7ab1e5297c72a4
Clementterme <clement.terme.simplon@gmail.com>
2026-01-07 16:01:25 +0100

    Mise à jour des produits dans le starter-project

---
+++ app/data.php
@@ -0,0 +1,16 @@
+<?php
+
+$products = [
+    ["name" => "T-shirt Manches Longues", "price" => 19.99, "stock" => 41, "category" => "Vêtements", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "T-shirt Manches Courtes", "price" => 18.99, "stock" => 0, "category" => "Vêtements", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Jean Délavé", "price" => 59.99, "stock" => 46, "category" => "Vêtements", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Jean Noir", "price" => 49.99, "stock" => 84, "category" => "Vêtements", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Pull Laine", "price" => 64.99, "stock" => 65, "category" => "Vêtements", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Ceinture Fourrure", "price" => 17.99, "stock" => 90, "category" => "Accessoires", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Ceinture Cuir", "price" => 34.99, "stock" => 78, "category" => "Accessoires", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Montre Bracelet Cuir", "price" => 189.99, "stock" => 45, "category" => "Accessoires", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Montre Bracelet Metal", "price" => 149.99, "stock" => 0, "category" => "Accessoires", "image" => "https://via.placeholder.com/200x200"],
+    ["name" => "Lunettes Teintées", "price" => 99.99, "stock" => 15, "category" => "Accessoires", "image" => "https://via.placeholder.com/200x200"],
+];
+
+?>
