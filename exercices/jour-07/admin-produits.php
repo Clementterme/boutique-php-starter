e607c49817314dab1512fa7fea7e5b6fb21c1fb5
Clementterme <clement.terme.simplon@gmail.com>
2026-01-15 16:47:08 +0100

    Exercice 6 commencé - Boutons Ajouter et Supprimer fonctionnels

---
+++ exercices/jour-07/admin-produits.php
@@ -0,0 +1,107 @@
+<h2>Ajouter un produit</h2>
+
+<form method="post" action="">
+    <label>
+        Nom :
+        <input type="text" name="name" required>
+    </label>
+    <br><br>
+
+    <label>
+        Prix :
+        <input type="number" name="price" step="0.01" min="0" required>
+    </label>
+    <br><br>
+
+    <label>
+        Stock :
+        <input type="number" name="stock" min="0" required>
+    </label>
+    <br><br>
+
+    <button type="submit" name="ajouter">Ajouter</button>
+</form>
+
+<?php
+
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=boutique;charset=utf8mb4",
+    "dev", "dev",
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+);
+
+if (isset($_POST["ajouter"])) {
+    $stmt = $pdo->prepare("INSERT INTO products (name, price, stock) VALUES (?, ?, ?)");
+    $stmt->execute([$_POST["name"], $_POST["price"], $_POST["stock"]]);
+    header("Location: admin-produits.php");
+    exit;
+}
+
+if(isset($_POST["editForm"])) {
+
+?>
+
+<form method="post" action="">
+    <label>
+        Nom :
+        <input type="text" name="name" required>
+    </label>
+    <br><br>
+
+    <label>
+        Prix :
+        <input type="number" name="price" step="0.01" min="0" required>
+    </label>
+    <br><br>
+
+    <label>
+        Stock :
+        <input type="number" name="stock" min="0" required>
+    </label>
+    <br><br>
+
+    <button type="submit" name="modifier" value="<?= $product["id"] ?>">Modifier</button>
+</form>
+
+<?php
+
+}
+
+if(isset($_POST["modifier"])) {
+    $stmt = $pdo->prepare("UPDATE products SET name = ?, price = ?, stock = ? WHERE id = ?");
+
+    $stmt->execute([$_POST['name'], $_POST['price'], $_POST['stock'], $_POST['id']]);
+
+    header("Location: admin-produits.php");
+    exit;
+    }
+
+// SELECT avec paramètre
+$stmt = $pdo->prepare("SELECT * FROM products");
+$stmt->execute();
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($products as $product) {
+
+?>
+    <div>
+        <h3><?= $product['name'] ?></h3>
+        <p>Prix : <?= $product['price'] ?> €</p>
+        <p>Stock restant : <?= $product['stock'] ?></p>
+        <form method="post">
+            <button type="submit" name="editForm" value="<?= $product["id"] ?>">Modifier</button>
+        </form>
+        <form method="post">
+            <button type="submit" name="supprimer" value="<?= $product["id"] ?>" onclick="return confirm('Supprimer ce produit ?')">Supprimer</button>
+        </form>
+    </div>
+<?php
+
+}
+
+if(isset($_POST["supprimer"])) {
+    $stmt = $pdo->prepare("DELETE FROM products WHERE id = ?");
+    $stmt->execute([$_POST["supprimer"]]);
+    header("Location: admin-produits.php");
+    exit;
+}
